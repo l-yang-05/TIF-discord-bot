c7fadb53598f3d571c55511d250db7291555b988
Lucy Yang <8395101kktss@gmail.com>
2020-04-19 21:33:28 -0400

    Added sending file funcitonality to bot

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 require('dotenv').config()
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const { Client, MessageAttachment } = require('discord.js')
+const client = new Client()
+const fetch = require('node-fetch')
 
 const token = process.env.token
 
+client.login(token);
+
+let date = new Date()
+let day = date.getDay()
+
+var userList = [];
+const fetchUsers = (arr) => {
+
+    for (let i = 0; i < arr.length; i++) {
+        client.users.fetch(arr[i])
+            .then(data => {
+                userList.push(data);
+                return userList
+            })
+            .catch(err => { console.log(err) })
+    }
+    return userList
+}
+
+// fetchUsers(['193451327949504512', '151863991352819712'])
+
+
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`)
+    var testChannel = client.channels.cache.find(channel => channel.name === 'general')
+    console.log(`Bot is in ${testChannel}`)
+
 })
 
-// client.on('message', msg => {
-
-// });
-
-client.login(token);
+client.on('message', async message => {
+    try {
+        // const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+        const file = new MessageAttachment('https://i.pinimg.com/236x/b5/98/a1/b598a12a1d6b2921edecd0fea5089cc7.jpg')
+        message.author.send(file).catch(err => console.log(err));
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
